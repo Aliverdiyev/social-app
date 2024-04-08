c52d321dcc546c55ff57a5c8170fbd266861e41c
Elvin <aliverdiyevelvin@mail.ru>
2024-04-08 14:04:26 +0400

    created class tables

---
+++ src/main/java/com/social/entities/User.java
@@ -0,0 +1,26 @@
+package com.social.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String country;
+    private LocalDate birthday;
+}
